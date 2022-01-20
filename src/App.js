93b5a59009a9e320becd0bd6d59a79a5c96f0a9c
Vittorio Rivabella <vittoriorivabella@Vittorios-MBP.homenet.telecomitalia.it>
2022-01-20 17:07:18 +0100

    adds react app boilerplates

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [blockchain, setBlockchain] = useState()
+  const [owner, setOwner] = useState()
+  const [contractAddress, setContractAddress] = useState()
+  const [NFTs, setNFTs] = useState()
+
+  const fetchNFTS = async () => {
+    if(owner){
+      let data;
+      if(contractAddress){
+         data = await fetch(`https://eth-mainnet.alchemyapi.io/L9_nj7QcxFpgM9N-6EGfuUowlpWmA9py/v1/getNFTs?owner=${owner}&contractAddresses%5B%5D=${contractAddress}`).then(data => data.json())
+
+      }else{
+        data = await fetch(`https://eth-mainnet.alchemyapi.io/L9_nj7QcxFpgM9N-6EGfuUowlpWmA9py/v1/getNFTs?owner=${owner}`).then(data => data.json())
+      }
+
+      const NFTs = await Promise.all(data.ownedNfts.map(async (NFT) => {
+        const image = await fetch(`https://eth-mainnet.alchemyapi.io/L9_nj7QcxFpgM9N-6EGfuUowlpWmA9py/v1/getNFTMetadata?contractAddress=${NFT.contract.address}&tokenId=${NFT.id.tokenId}&tokenType=erc721`).then(data => data.json())
+        return {
+          contractAddress: NFT.contract.address,
+          id: NFT.id.tokenId,
+          image: image.media.uri ? image.media.uri : "https://via.placeholder.com/500",
+        }
+      }))
+      setNFTs(NFTs)
+    }
+  }
+
+  return (
+    <div className="App">
+      <div>
+      <h1>
+          Alchemy NFT Explorer
+        </h1>
+
+        <p>Lorem ipsum dolor sit amet your wallet address</p>
+      </div>
+        
+        <input onChange={(e) => setOwner(e.target.value)} placeholder='Insert your wallet address'></input>
+        <input onChange={(e) => setContractAddress(e.target.value)} placeholder='Insert NFT address (optional)'></input>
+      
+        <button onClick={fetchNFTS}>Search</button>
+        <div>
+          {NFTs && NFTs.map(NFT => {
+            console.log(NFT)
+            return(
+              <div>
+                {NFT.image && <img key={NFT.id}src={NFT.image}></img>}
+              </div>
+            )
+          })}
+        </div>
+    </div>
+  );
+}
+
+export default App;
