c4fcb95926272f83c54f2b7dbca90d0f123b40f7
Itamar Rubinski <itamarr.dev@gmail.com>
2022-02-21 20:47:12 +0200

    fix ChainSelector radio labels

--- src/components/chainSelector.jsx
+++ src/components/chainSelector.jsx
@@ -2,21 +2,21 @@ const ChainSelector = ({setBlockchain, chain}) => {
     return (
         <div className='flex gap-12 items-center text-white my-4'>
             <div>
-                <input onChange={(e) => setBlockchain(e.target.value)} className='mr-1' type="radio" name='blockchain' value="Ethereum" checked={chain == "Ethereum"} />
-                <label htmlFor="Eth">Ethereum</label>
+                <input onChange={(e) => setBlockchain(e.target.value)} className='mr-1' type="radio" name='blockchain' value="Ethereum" id="chain-eth" checked={chain == "Ethereum"} />
+                <label htmlFor="chain-eth">Ethereum</label>
             </div>
             <div>
-                <input onChange={(e) => setBlockchain(e.target.value)} className='mr-1' type="radio" name='blockchain' value="Rinkeby" checked={chain == "Rinkeby"} disabled />
-                <label htmlFor="Rinkeby">Rinkeby</label>
+                <input onChange={(e) => setBlockchain(e.target.value)} className='mr-1' type="radio" name='blockchain' value="Rinkeby" id="chain-rinkeby" checked={chain == "Rinkeby"} disabled />
+                <label htmlFor="chain-rinkeby">Rinkeby</label>
             </div>
 
             <div>
-                <input onChange={(e) => setBlockchain(e.target.value)} className='mr-1' type="radio" name='blockchain' value="Polygon" checked={chain == "Polygon"} />
-                <label htmlFor="Polygon">Polygon</label>
+                <input onChange={(e) => setBlockchain(e.target.value)} className='mr-1' type="radio" name='blockchain' value="Polygon" id="chain-polygon" checked={chain == "Polygon"} />
+                <label htmlFor="chain-polygon">Polygon</label>
             </div>
             <div>
-                <input onChange={(e) => setBlockchain(e.target.value)} className='mr-1' type="radio" name='blockchain' value="Mumbai" checked={chain == "Mumbai"} />
-                <label htmlFor="Mumbai">Mumbai</label>
+                <input onChange={(e) => setBlockchain(e.target.value)} className='mr-1' type="radio" name='blockchain' value="Mumbai" id="chain-mumbai" checked={chain == "Mumbai"} />
+                <label htmlFor="chain-mumbai">Mumbai</label>
             </div>
         </div>
     )
